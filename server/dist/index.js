c4bc2f03d6d31575e63f5efb01169b00df9aaab7
iamraselmolla <raselmolla6336@gmail.com>
2025-10-20 12:04:45 +0600

    project init

---
+++ server/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Server is running on http://localhost:4000..");
+//# sourceMappingURL=index.js.map
---
+++ server/src/index.ts
@@ -0,0 +1,3 @@
+import {} from "@mikro-orm/core";
+
+console.log("Server is running on http://localhost:4000..");
